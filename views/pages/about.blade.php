6089362ee81e645c547fcaa617b8eeda11f444e5
sadnesszxc <88449346+sadnesszxc@users.noreply.github.com>
2021-08-09 16:03:21 +0800

    Add files via upload

---
+++ views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>{{$title}}</h1>
+ <p>Abegail Acaso, Alexes Sangilan, Louis Gloriani, Justine Jabinar</p>
+@endsection
---
+++ views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+    <h1>Welcome!</h1>
+    <p>labratory 1</p>
+    <p><a class="btn btn-primary btn-lg" href="login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+</div>
+    @endsection
+
+ 
