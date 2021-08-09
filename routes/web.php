4de6aab674416b7b01d449b991191e02ae15bb64
sadnesszxc <88449346+sadnesszxc@users.noreply.github.com>
2021-08-09 16:03:47 +0800

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+ //Route::get('/about', function () {
+    //return view('pages.about');
+
+Route::get('/', [PagesController::class, 'index']); 
+Route::get('/about', [PagesController::class, 'about']); 
+Route::get('/services', [PagesController::class, 'services']); 
+
+Route::resource('posts', 'PostsController');
